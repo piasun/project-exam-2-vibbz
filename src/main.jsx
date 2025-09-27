2dfe841f60776b2efc75487f8b4152fdf9c64151
piasun <77275966+piasun@users.noreply.github.com>
2025-09-27 13:15:07 +0200

    Fixes #8: Set up global AuthContext and wrap App in provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './features/auth/AuthContext';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -9,6 +10,8 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <AuthProvider>
     <App />
+    </AuthProvider>
   </BrowserRouter>
 );
