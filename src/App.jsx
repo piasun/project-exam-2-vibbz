098764efb68b03c0b7286e744664020e8eb0da5c
piasun <77275966+piasun@users.noreply.github.com>
2025-09-27 14:15:31 +0200

    updated app with correct imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 // src/App.jsx
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import SinglePost from './pages/SinglePost';
+import CreatePost from './pages/CreatePost';
 
 export default function App() {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/profiles/:name" element={<Profile />} />
+      <Route path="/posts/:id" element={<SinglePost />} />
+      <Route path="/posts/create" element={<CreatePost />} />
     </Routes>
+    </>
   );
 }
