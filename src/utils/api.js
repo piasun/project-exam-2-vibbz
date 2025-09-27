88869f0c16f4de47220ac1be53e25cbd8a73625e
piasun <77275966+piasun@users.noreply.github.com>
2025-09-27 12:53:58 +0200

    modified utils/api (#7)

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const API_BASE = 'https://nf-api.onrender.com/api/v1/social';
-
 export const api = axios.create({
-  baseURL: API_BASE,
+  baseURL: 'https://nf-api.onrender.com/api/v1/social',
 });
